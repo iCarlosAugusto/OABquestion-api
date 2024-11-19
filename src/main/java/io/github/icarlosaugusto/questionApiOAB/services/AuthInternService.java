3273dc4896592a6ebf628ded0755b7b71afd1dc0
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-11-19 18:36:56 -0300

    feat: Service responsavel em fazer o login e sigup do interno do sistema

--- src/main/java/io/github/icarlosaugusto/questionApiOAB/services/AuthInternService.java
+++ src/main/java/io/github/icarlosaugusto/questionApiOAB/services/AuthInternService.java
@@ -4,19 +4,55 @@ import io.github.icarlosaugusto.questionApiOAB.dtos.AuthDTO;
 import io.github.icarlosaugusto.questionApiOAB.entities.User;
 import io.github.icarlosaugusto.questionApiOAB.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
-public class AuthService {
+public class AuthInternService {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder; 
+    
+
+    /**
+     * Autentica um usuário com base em e-mail e senha.
+     *
+     * @param authDTO Objeto contendo e-mail e senha do usuário.
+     * @return O usuário autenticado, ou null se as credenciais forem inválidas.
+     */
     public User authenticate(AuthDTO authDTO) {
-        return this.userRepository.findByEmailAndPassword(authDTO.getEmail(), authDTO.getPassword()).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NO_CONTENT, "User not found")
-        );
+        Optional<User> userOpt = userRepository.findByEmail(authDTO.getEmail());
+
+        if (!userOpt.isPresent()) 
+        	return null;
+        
+        User user = userOpt.get();
+        if (passwordEncoder.matches(authDTO.getPassword(), user.getPassword())) 
+        	return user;
+
+        return null; 
+    }
+
+    /**
+     * Registra um novo usuário com base em e-mail e senha.
+     *
+     * @param authDTO Objeto contendo e-mail e senha do usuário.
+     * @return O usuário recém-registrado.
+     */
+    public User register(AuthDTO authDTO) {
+        String encodedPassword = passwordEncoder.encode(authDTO.getPassword());
+
+        User newUser = new User();
+        newUser.setEmail(authDTO.getEmail());
+        newUser.setPassword(encodedPassword);
+        newUser.setName(authDTO.getName());
+
+        return userRepository.save(newUser);
     }
 }
+
