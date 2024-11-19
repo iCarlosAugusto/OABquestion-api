36885331eda61ca95e6eb652dfe9adf4aa866e80
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-11-19 18:33:39 -0300

    removed: imports não usados

--- src/main/java/io/github/icarlosaugusto/questionApiOAB/repositories/DisciplineRepository.java
+++ src/main/java/io/github/icarlosaugusto/questionApiOAB/repositories/DisciplineRepository.java
@@ -3,7 +3,5 @@ package io.github.icarlosaugusto.questionApiOAB.repositories;
 import io.github.icarlosaugusto.questionApiOAB.entities.Discipline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface DisciplineRepository extends JpaRepository<Discipline, Long> {
 }
