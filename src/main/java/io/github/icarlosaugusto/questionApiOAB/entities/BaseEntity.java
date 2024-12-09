dcb017ff6ff9a64d6fd7ee94ea8c91928e64bd9f
iCarlosAugusto <augusto.dev@proton.me>
2024-12-09 01:32:38 -0300

    criado base entity

---
+++ src/main/java/io/github/icarlosaugusto/questionApiOAB/entities/BaseEntity.java
@@ -0,0 +1,23 @@
+package io.github.icarlosaugusto.questionApiOAB.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @CreatedDate
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
